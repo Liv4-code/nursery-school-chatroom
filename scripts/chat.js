5688b9ec2a66990120baed4c0b132216c30a4b7a
livi <scholtz.ice@gmail.com>
2024-01-19 20:49:33 +0200

    Created methods for updating username & room

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection("chats");
+        this.unsub;
     }
     async addChat(message) {
         // format the chat object
@@ -24,18 +25,41 @@ class Chatroom {
     }
 
     getChats(callback) {
-        this.chats.onSnapshot((snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                if (change.type === "added") {
-                    callback(change.doc.data());
-                } else {
-                }
+        this.unsub = this.chats
+            .where("room", "==", this.room)
+            .orderBy("created_at")
+            .onSnapshot((snapshot) => {
+                snapshot.docChanges().forEach((change) => {
+                    if (change.type === "added") {
+                        callback(change.doc.data());
+                    }
+                });
             });
-        });
+    }
+
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log("room changed");
+        if (this.unsub) {
+            this.unsub();
+        }
     }
 }
 
 const chatroom = new Chatroom("education", "suzy");
+
 chatroom.getChats((data) => {
     console.log(data);
 });
+
+setTimeout(() => {
+    chatroom.updateRoom("meals");
+    chatroom.updateName("lesli");
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat("hey everybody :)");
+}, 3000);
